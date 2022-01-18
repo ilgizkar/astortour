2ce7a1a3912dc1854b9980b941dcecb8bead7b8f
ILGIZ <karabaev.il@yandex.ru>
2022-01-18 23:36:08 +0300

    First

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,3 +29,30 @@ Vue.component('carousel-component', require('./components/CarouselComponent.vue'
 const app = new Vue({
     el: '#app',
 });
+
+$("#scroll_to_form").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#form").offset().top
+    }, 100);
+});
+
+var form = document.getElementById("form");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        form.reset()
+        alert('Ваша заявка отпревлена!')
+    }).catch(error => {
+        alert('Произошли ошибки!')
+    });
+}
+form.addEventListener("submit", handleSubmit);
